5941ae2e52be84ab361103186ca9f16a037efe6a
Leonardo Razovic <leorazzo@gmail.com>
2021-04-11 18:40:39 +0200

    feat: decompression with a local file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,25 @@
-use flate2::read::GzDecoder;
-use std::io::prelude::*;
+use compress::zlib;
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
 
 fn main() {
-    println!("Hello, world!");
+    let stream = File::open(&Path::new("obj2")).unwrap();
+    let mut decompressed = Vec::new();
+    match zlib::Decoder::new(stream).read_to_end(&mut decompressed) {
+        Ok(_) => {
+            let obj = String::from_utf8(decompressed);
+            match obj {
+                Ok(str) => {
+                    println!("{}", str)
+                }
+                Err(_) => {
+                    eprintln!("Eror in UTF-8 Decoding")
+                }
+            }
+        }
+        Err(_) => {
+            eprintln!("Error in Zlib Decompressing")
+        }
+    }
 }
