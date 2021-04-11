cd7497f0b8be4cac3ce71bce872cb9d65a97512c
Leonardo Razovic <leorazzo@gmail.com>
2021-04-11 19:46:20 +0200

    feat: Parse the name of the file and open it

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
+use clap::{App, Arg};
 use compress::zlib;
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
 
 fn main() {
-    let stream = File::open(&Path::new("obj2")).unwrap();
+    let matches = App::new("Decode Git Object")
+        .version("0.1.0")
+        .author("Leonardo Razovic <lrazovic@gmail.com>")
+        .about("Teaches argument parsing")
+        .arg(
+            Arg::with_name("file")
+                .required(true)
+                .short("f")
+                .long("file")
+                .takes_value(true)
+                .help("The object file to decode"),
+        )
+        .get_matches();
+    let filename = matches.value_of("file").unwrap();
+    let stream = File::open(&Path::new(filename)).unwrap();
     let mut decompressed = Vec::new();
     match zlib::Decoder::new(stream).read_to_end(&mut decompressed) {
         Ok(_) => {
